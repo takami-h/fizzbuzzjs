2d90e2ac714a1b2073e82c0ff4ec2d64ae9a30bb
Takami Hirata <26153825+takami-h@users.noreply.github.com>
2022-04-01 10:24:05 +0900

    feat: enable type check based on jsdoc

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -2,6 +2,11 @@ const divisibleByN = (i, n) => i % n == 0;
 const divisibleBy3 = (i) => divisibleByN(i, 3);
 const divisibleBy5 = (i) => divisibleByN(i, 5);
 
+/**
+ * returns Fizz, Buzz, Fizz Buzz, or number based on number passed.
+ * @param {number} number 
+ * @returns {string}
+ */
 function fizzbuzz(number) {
   if (!Number.isInteger(number)) throw `${number} is not an integer.`;
 
